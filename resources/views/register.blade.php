058598e79704fa227eadb21bc6c52184bf37ac91
nasywaraichanah <nasywaraichanah@student.uns.ac.id>
2025-02-17 10:06:32 +0700

    edit register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome - SIMANTRA</title>
+    <title>SIMANTRA - Register</title>
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
 <body>
     <div class="login-container">
-        <div class="left-section">
+        <div class="right-section">
             <div class="header">
                 <div class="logo">
                 <img src="{{ asset('image/logo_boyolali.png') }}" alt="Logo">
@@ -21,32 +21,48 @@
 
             <button class="btn-signup">SIGN UP</button>
         </div>
-        <div class="right-section">
+        <div class="left-section">
             <div class="contact-info">
                 <p>
                     <i class="fas fa-envelope"></i> bkpsdm@boyolali.go.id 
                     <i class="fas fa-phone"></i> (0276) 321005
                 </p>
             </div>
-            <div class="login-form">
+            <div class="register-form">
                 <div class="judul-website">SIMANTRA</div>
                 <div class="judul-website-2">Sistem Manajemen Inventaris Terpadu</div>
                 @if ($errors->any())
                     <div class="error-message">
-                        <strong>{{ $errors->first('login') }}</strong>
+                        <strong>{{ $errors->first('register') }}</strong>
                     </div>
                 @endif
-                <form action="{{ url('/') }}" method="POST">
+                <form action="{{ url('register') }}" method="POST">
                     @csrf
                     <div class="input-group">
-                        <input type="text" name="nip" placeholder="Nomor Induk Pegawai" required>
+                        <input type="text" name="name" placeholder="Full Name" required>
                         <i class="fas fa-user"></i>
+                    </div>
+                    <div class="input-group">
+                        <input type="email" name="email" placeholder="Email" required>
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="input-group">
+                        <input type="text" name="nip" placeholder="Nomor Induk Pegawai" required>
+                        <i class="fas fa-id-badge"></i>
                     </div>
                     <div class="input-group">
                         <input type="password" name="password" placeholder="Password" required>
                         <i class="fas fa-lock"></i>
                     </div>
-                    <button type="submit" class="btn-signin">SIGN IN</button>
+                    <div class="input-group">
+                        <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="input-group">
+                    <label for="profile_picture">Upload Foto Profil</label>
+                        <input type="file" name="profile_picture" accept="image/*" placeholder="Foto Profil" required>
+                    </div>
+                    <button type="submit" class="btn-signin">REGISTER</button>
                 </form>
             </div>
         </div>
