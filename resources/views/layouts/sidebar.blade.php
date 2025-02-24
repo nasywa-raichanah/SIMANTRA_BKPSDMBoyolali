c0f0444b7bcfa9414f3efbec8f871a9168eb998e
nasywaraichanah <nasywaraichanah@student.uns.ac.id>
2025-02-24 10:32:51 +0700

    perbaiki sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,14 +4,15 @@
         <button class="menu-toggle">☰</button>
     </div>
     <ul class="menu">
-        <li><a href="{{ route('dashboard') }}"><i class="icon">🏠︎</i> <span class="menu-text">Dashboard</span></a></li>
-        <li><a href="{{ route('aset') }}"><i class="icon">📦</i> <span class="menu-text">Aset</span></a></li>
+        <li><a href="{{ route('dashboard') }}"><img src="{{ asset('image/dashboard.png') }}" class="icon"> <span class="menu-text">Dashboard</span></a></li>
+        <li><a href="{{ route('aset.index') }}"><img src="{{ asset('image/aset.png') }}" class="icon"> <span class="menu-text">Aset</span></a></li>
+        <li><img src="{{ asset('image/file.png') }}" class="icon"> <span class="menu-text">Laporan</span></li>
         <li>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
             <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                <i class="icon">🔄</i> <span class="menu-text">Logout</span>
+                <img src="{{ asset('image/logout.png') }}" class="icon"><span class="menu-text">Logout</span>
             </a>
         </li>
 
